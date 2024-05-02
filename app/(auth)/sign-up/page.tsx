393bc3f787b8cf0e5151227b7419020bffcb1cb4
khadija-kk1902086 <88961262+khadija-kk1902086@users.noreply.github.com>
2024-05-02 15:01:08 +0300

    content deleted

--- app/(auth)/sign-up/page.tsx
+++
@@ -1,8 +0,0 @@
-import SignUpForm from '@/components/SignUpForm';
-import React from 'react'
-
-const page = () => {
-  return <div className='w-full' ><SignUpForm/></div>
-};
-
-export default page
--- app/flash-cards/layout.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import "../../styles/flashcard.css"
-const layout = ({ children }) => {
-  return <div>{children}</div>;
-};
-
-export default layout
